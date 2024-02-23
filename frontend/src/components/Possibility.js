9dd6f761faaf5eb1cf6f2d37d569b82795b16183
sohamt-21 <sohamtak0403@gmail.com>
2024-02-23 09:54:34 +0530

    Changed form CSS

--- frontend/src/components/Possibility.js
+++ frontend/src/components/Possibility.js
@@ -9,7 +9,7 @@ const Possibility = () => (
         </div>
         <div className="possibility-content">
             <h4 id='possibility-h4'>Inbuilt Meet Feature for Interviews </h4>
-            <h1 style={{fontSize:'30px',fontWeight:'500',lineHeight:'45px',margin:'1rem 0',background: 'linear-gradient(89.97deg, #134580 1.84%, #04001D 102.67%)',color:'aliceblue'}}>Crate and Take meets</h1>
+            <h1 style={{fontSize:'30px',fontWeight:'500',lineHeight:'45px',margin:'1rem 0',background: 'linear-gradient(89.97deg, #134580 1.84%, #04001D 102.67%)',color:'aliceblue'}}>Create and Take meets</h1>
             <p id='possibility-p'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea repellendus enim dolore ipsam, quo consequuntur ad quaerat necessitatibus asperiores reprehenderit? Ratione odio repellat dolores?</p>
             <button id='skipDiv-button'>Sign In</button>
         </div>
